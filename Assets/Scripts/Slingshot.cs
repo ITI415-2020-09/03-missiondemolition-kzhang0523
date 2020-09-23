371165c9512d282b9d4da31219bab8621a6db0ea
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-09-23 17:10:57 -0400

    Instantiate a Projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter()
     {
@@ -19,5 +27,17 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //Player has pressed the mouse button while over slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //start it at the launchpoint
+        projectile.transform.position = launchPos;
+        //Set it to iskinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
