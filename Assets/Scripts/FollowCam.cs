81b87777fa428fd43ae9a4c536350a640cb43be4
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-09-23 18:02:31 -0400

    Fix Camera Issues

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,6 +5,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; //static point of interest
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -19,8 +23,15 @@ public class FollowCam : MonoBehaviour
 
         //get position of the poi
         Vector3 destination = POI.transform.position;
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+
+        //set the orthographic size of the cam to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10;
 
 
     }
